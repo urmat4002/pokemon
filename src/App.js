55d399914e991ed3690935b58592ade9edf5ae0f
urmat4002 <urmat4002@gmail.com>
2023-11-04 17:44:19 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import {Card, Image} from 'react-bootstrap'
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ ')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results)
+        setData(data.results)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        background: '#FFFFE0',
+        flexWrap: 'wrap',
+      }}
+    >
+      {data &&
+        data.map((item, index) => (
+          <Card
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              background: '#00FFFF',
+              width: '200px',
+              borderRadius: '5px',
+              border: '1px solid black',
+              margin: '5px',
+            }}
+          >
+            <Image src={item.url} />
+            <Card.Text>{item.name}</Card.Text>
+          </Card>
+        ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
